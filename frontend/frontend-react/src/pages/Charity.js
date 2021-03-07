cfebc69f178979f47c901389bb8dfd02f796f6d3
Chris Koehne <cdkoehne@gmail.com>
2021-03-07 02:38:09 -0500

    replaced some lorem ipsum text

--- frontend/frontend-react/src/pages/Charity.js
+++ frontend/frontend-react/src/pages/Charity.js
@@ -8,165 +8,193 @@ import roomToRead from '../img/roomToReadIcon.svg';
 import unitedWay from '../img/unitedWayIcon.svg';
 import waterOrg from '../img/waterOrgIcon.svg';
 
-import { charitySelect, charityStatus } from '../connections/userConnections'
+import { charitySelect, charityStatus } from '../connections/userConnections';
 
 class Charity extends React.Component {
   constructor() {
     super();
     this.state = {
-		loaded: false,
-      	charity: 'none',
+      loaded: false,
+      charity: 'none',
     };
   }
 
   async componentWillMount() {
-		if (window.localStorage.hasOwnProperty("jwt") === false) {
-			this.props.history.push('/');
-			return;
-		}
-		let result = await charityStatus(window.localStorage.getItem("jwt"))
-		this.setState({
-			loaded: true,
-			charity: result.message.charity
-		})
+    if (window.localStorage.hasOwnProperty('jwt') === false) {
+      this.props.history.push('/');
+      return;
+    }
+    let result = await charityStatus(window.localStorage.getItem('jwt'));
+    this.setState({
+      loaded: true,
+      charity: result.message.charity,
+    });
   }
 
-	handleClick = async (e) => {
-		await console.log(e)
-		await charitySelect(window.localStorage.getItem("jwt"), e.target.id)
-		await this.setState({
-			charity: e.target.id
-		})
+  handleClick = async (e) => {
+    await console.log(e);
+    await charitySelect(window.localStorage.getItem('jwt'), e.target.id);
+    await this.setState({
+      charity: e.target.id,
+    });
+  };
 
-	}
+  render() {
+    console.log(this.state);
+    let bagClass =
+      this.state.charity === 'boys and girls club' ? 'selected' : '';
+    let bailClass = this.state.charity === 'bail project' ? 'selected' : '';
+    let feedingClass =
+      this.state.charity === 'feeding america' ? 'selected' : '';
+    let roomClass = this.state.charity === 'room to read' ? 'selected' : '';
+    let unitedClass = this.state.charity === 'united way' ? 'selected' : '';
+    let waterClass = this.state.charity === 'water.org' ? 'selected' : '';
 
-	render() {
-		console.log(this.state)
-		let bagClass = (this.state.charity === 'boys and girls club')?'selected':''
-		let bailClass = (this.state.charity === 'bail project')?'selected':''
-		let feedingClass = (this.state.charity === 'feeding america')?'selected':''
-		let roomClass = (this.state.charity === 'room to read')?'selected':''
-		let unitedClass = (this.state.charity === 'united way')?'selected':''
-		let waterClass = (this.state.charity === 'water.org')?'selected':''
+    let mainContent;
+    if (this.state.loaded) {
+      mainContent = (
+        <div>
+          <div class='cardRow'>
+            <div class='col'>
+              <div class={'card ' + bagClass}>
+                <a target='_blank' href='https://www.bgca.org'>
+                  <img class='charityIcon' src={bagClub} />
+                </a>
+                <p>
+                  Boys & Girls Clubs of America seeks to enable all young
+                  people, especially those who have the greatest need, to reach
+                  their full potential as productive, caring, responsible
+                  citizens.{' '}
+                </p>
+                <button
+                  class='btnDarkGradient'
+                  id='boys and girls club'
+                  onClick={this.handleClick}
+                >
+                  select
+                </button>
+              </div>
+            </div>
+            <div class='col'>
+              <div class={'card ' + bailClass}>
+                <a target='_blank' href='https://bailproject.org'>
+                  <img class='charityIcon' src={bailProject} />
+                </a>
+                <p>
+                  The Bail Project combats mass incarceration by disrupting the
+                  money bail system—one person at a time. 100% of online
+                  donations are used to bring people home
+                </p>
+                <button
+                  class='btnDarkGradient'
+                  id='bail project'
+                  onClick={this.handleClick}
+                >
+                  select
+                </button>
+              </div>
+            </div>
+            <div class='col'>
+              <div class={'card ' + feedingClass}>
+                <a target='_blank' href='https://www.feedingamerica.org'>
+                  <img class='charityIcon' src={feedingAmerica} />
+                </a>
+                <p>
+                  What began in 1979 as a clearinghouse for national food
+                  donations is now the nation’s largest domestic hunger-relief
+                  organization—a powerful and efficient network of 200 food
+                  banks across the country.
+                </p>
+                <button
+                  class='btnDarkGradient'
+                  id='feeding america'
+                  onClick={this.handleClick}
+                >
+                  select
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class='cardRow'>
+            <div class='col'>
+              <div class={'card ' + roomClass}>
+                <a target='_blank' href='https://www.roomtoread.org'>
+                  <img class='charityIcon' src={roomToRead} />
+                </a>
+                <p>
+                  Room to Read seeks to transform the lives of millions of
+                  children in low-income communities by focusing on literacy and
+                  gender equality in education.{' '}
+                </p>
+                <button
+                  class='btnDarkGradient'
+                  id='room to read'
+                  onClick={this.handleClick}
+                >
+                  select
+                </button>
+              </div>
+            </div>
+            <div class='col'>
+              <div class={'card ' + unitedClass}>
+                <a target='_blank' href='https://www.unitedway.org'>
+                  <img class='charityIcon' src={unitedWay} />
+                </a>
+                <p>
+                  United Way’s mission is to improve lives by mobilizing the
+                  caring power of communities around the world to advance the
+                  common good.
+                </p>
+                <button
+                  class='btnDarkGradient'
+                  id='united way'
+                  onClick={this.handleClick}
+                >
+                  select
+                </button>
+              </div>
+            </div>
+            <div class='col'>
+              <div class={'card ' + waterClass}>
+                <a target='_blank' href='https://www.water.org'>
+                  <img class='charityIcon' src={waterOrg} />
+                </a>
+                <p>
+                  Water.org is a global nonprofit organization working to bring
+                  water and sanitation to the world in a way that's safe,
+                  accessible, and cost-effective.
+                </p>
+                <button
+                  class='btnDarkGradient'
+                  id='water.org'
+                  onClick={this.handleClick}
+                >
+                  select
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      mainContent = (
+        <div>
+          <div class='cardRow'></div>
+          <div class='cardRow'></div>
+        </div>
+      );
+    }
 
-		let mainContent
-		if (this.state.loaded) {
-			mainContent = (
-				<div>
-					<div class="cardRow">
-						<div class="col">
-								<div class={"card "+ bagClass}>
-									<a target="_blank" href="https://www.bgca.org">
-										<img class="charityIcon" src={bagClub}/>
-									</a>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-									</p>
-									<button class="btnDarkGradient" id="boys and girls club"  onClick={this.handleClick}>
-										select
-									</button>
-								</div>
-						</div>
-						<div class="col">
-								<div class={"card "+ bailClass}>
-									<a target="_blank" href="https://bailproject.org">
-										<img class="charityIcon" src={bailProject}/>
-									</a>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-									</p>
-									<button class="btnDarkGradient" id="bail project"  onClick={this.handleClick}>
-										select
-									</button>
-								</div>
-						</div>
-						<div class="col">
-								<div class={"card "+ feedingClass}>
-									<a target="_blank" href="https://www.feedingamerica.org">
-										<img class="charityIcon" src={feedingAmerica}/>
-									</a>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-									</p>
-									<button class="btnDarkGradient" id="feeding america"  onClick={this.handleClick}>
-										select
-									</button>
-								</div>
-						</div>
-					</div>
-					<div class="cardRow">
-						<div class="col">
-								<div class={"card "+ roomClass}>
-									<a target="_blank" href="https://www.roomtoread.org">
-										<img class="charityIcon" src={roomToRead}/>
-									</a>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-									</p>
-									<button class="btnDarkGradient" id="room to read"  onClick={this.handleClick}>
-										select
-									</button>
-								</div>
-						</div>
-						<div class="col">
-								<div class={"card "+ unitedClass}>
-									<a target="_blank" href="https://www.unitedway.org">
-										<img class="charityIcon" src={unitedWay}/>
-									</a>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-									</p>
-									<button class="btnDarkGradient" id="united way"  onClick={this.handleClick}>
-										select
-									</button>
-								</div>
-						</div>
-						<div class="col">
-								<div class={"card "+ waterClass}>
-									<a target="_blank" href="https://www.water.org">
-										<img class="charityIcon" src={waterOrg}/>
-									</a>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-									</p>
-									<button class="btnDarkGradient" id="water.org"  onClick={this.handleClick}>
-										select
-									</button>
-								</div>
-						</div>
-					</div>
-				</div>
-			)
-		}
-		else {
-			mainContent = (
-				<div>
-					<div class="cardRow">
-
-					</div>
-					<div class="cardRow">
-						
-					</div>
-				</div>
-			)
-		}
-
-		return(
-			<div>
-				<Navbar />
-				<div class="charityCenter">
-					<h1 class="landingH1">
-						Pick a cause that matters to you.
-					</h1>
-				</div>
-				{ mainContent }
-				
-				
-		    </div>
-			
-		)
-	}
-
+    return (
+      <div>
+        <Navbar />
+        <div class='charityCenter'>
+          <h1 class='landingH1'>Pick a cause that matters to you.</h1>
+        </div>
+        {mainContent}
+      </div>
+    );
+  }
 }
 
 export default Charity;
--- frontend/frontend-react/src/pages/Home.js
+++ frontend/frontend-react/src/pages/Home.js
@@ -1,184 +1,174 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Switch,
-} from 'react-router-dom'
-import Navbar from '../components/Navbar'
-import { updateActivity, charityStatus } from "../connections/userConnections";
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import { updateActivity, charityStatus } from '../connections/userConnections';
 
 class Home extends React.Component {
-	constructor() {
-		super()
-		this.state = {
-			loaded: false,
-			initial: false,
-			active: false
-		}
-		console.log("constructor state", this.state)
-		this.transition = this.transition.bind(this)
-	}
+  constructor() {
+    super();
+    this.state = {
+      loaded: false,
+      initial: false,
+      active: false,
+    };
+    console.log('constructor state', this.state);
+    this.transition = this.transition.bind(this);
+  }
 
-	transition() {
-		let button = document.querySelector('.play-pause-button')
-		if (button !== null) {
-			button.classList.remove('paused', 'playing');
-			if (this.state.initial) {
-				button.classList.add("playing")
-			}
-			else {
-				button.classList.add("paused")
-			}
-			
-		}
-		document.querySelectorAll('.play-pause-button').forEach(button => {
-			button.addEventListener('click', e => {
-				if(button.classList.contains('playing')) {
-						button.classList.remove('paused', 'playing');
-						button.classList.add('paused');
-				} else {
-						if(button.classList.contains('paused')) {
-								button.classList.add('playing');
-						}
-				}
-				if(!button.classList.contains('paused')) {
-						button.classList.add('paused');
-				}
-			});
-		});
-	}
+  transition() {
+    let button = document.querySelector('.play-pause-button');
+    if (button !== null) {
+      button.classList.remove('paused', 'playing');
+      if (this.state.initial) {
+        button.classList.add('playing');
+      } else {
+        button.classList.add('paused');
+      }
+    }
+    document.querySelectorAll('.play-pause-button').forEach((button) => {
+      button.addEventListener('click', (e) => {
+        if (button.classList.contains('playing')) {
+          button.classList.remove('paused', 'playing');
+          button.classList.add('paused');
+        } else {
+          if (button.classList.contains('paused')) {
+            button.classList.add('playing');
+          }
+        }
+        if (!button.classList.contains('paused')) {
+          button.classList.add('paused');
+        }
+      });
+    });
+  }
 
-	
+  async componentWillMount() {
+    if (window.localStorage.hasOwnProperty('jwt') === false) {
+      this.props.history.push('/');
+    }
+    let result = await charityStatus(window.localStorage.getItem('jwt'));
 
-	async componentWillMount() {
-		if (window.localStorage.hasOwnProperty("jwt") === false) {
-			this.props.history.push("/")
-		}
-		let result = await charityStatus(window.localStorage.getItem("jwt"))
+    this.setState({
+      loaded: true,
+      initial: result.message.active,
+      active: result.message.active,
+    });
+    console.log('cDM state', this.state);
+    await this.transition();
+    console.log(this.props.history);
+  }
 
-		this.setState({
-			loaded: true,
-			initial: result.message.active,
-			active: result.message.active
-		})
-		console.log("cDM state", this.state)
-		await this.transition()
-		console.log(this.props.history)
-	}
+  handleClick = async () => {
+    await updateActivity(
+      window.localStorage.getItem('jwt'),
+      !this.state.active
+    );
+    this.setState({
+      active: !this.state.active,
+    });
+  };
 
-	handleClick = async () => {
-		await updateActivity(window.localStorage.getItem("jwt"), !this.state.active)
-		this.setState({
-			active: !this.state.active
-		})
-		
-	}
-	
-	render() {
-		console.log("render state", this.state)
-		let mainContent
-		if (this.state.loaded) {
-			mainContent = (
-				<div class="row">
-					<div class="col">
-					
-						
-							<h1 class="landingH1">
-								Building Karma on autopilot
-							</h1>
-							
-					
-						<p class="landingP">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-						</p>
-						<div class="buttons" onClick={this.handleClick}>
-							<button class="play-pause-button">
-									<i>P</i>
-									<i>l</i>
-									<i>a</i>
-									<i>y</i>
-									<i>use</i>
-							</button>
-						</div>
-					</div>
-					
-				</div>
-			)
-		}
-		else {
-			mainContent = (
-				<div class="row">
-					<div class="col">
-		          		
-		          	</div>
-				</div>
-			)
-		}
-		return(
-			<div>
-				<Navbar />
-		    <section class="landing">
-				{ mainContent }	
-				</section>
-				<section class="info">
-				<div class="row">
-					<div class="col bios">
-						<i class="fas fa-shield-alt"></i>
-						<h1>
-							Secure
-						</h1>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-						</p>
-					</div>
-					<div class="col">
-						<div class="bios">
-							<i class="fas fa-search-dollar"></i>
-							<h1>
-								Transparent
-							</h1>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-							</p>
-						</div>
-						
-					</div>
-					<div class="col bios">
-						<i class="fas fa-hand-holding-usd"></i>
-						<h1>
-							Non-profit
-						</h1>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-						</p>
-					</div>
-				</div>
-						
-				</section>
-				<div class="wave">
-					<svg viewBox="0 0 500 150" preserveAspectRatio="none" class="svgStyle">
-						<path d="M0.00,92.27 C216.83,192.92 304.30,8.39 500.00,109.03 L500.00,0.00 L0.00,0.00 Z" class="pathStyle"></path>
-					</svg>
-				</div>
-				<section class="moreInfo">
-					<div class="row">
-						<div class="col">	
-							<div class="slider">
-								<h1 class="landingH1">
-									Most major banks accepted
-								</h1>
-								<div class="slider-row"></div>
-							</div>
-						</div>
-					</div>
-					
-					
-				</section>
-		  </div>
-		  
-		)
-	}
+  isActiveText = () => {
+		console.log("here")
+    if (this.state.active) {
+      return <p class='landingP'>You currently have rounding-up donations on.</p>;
+    } else {
+      return (
+        <p class='landingP'>
+          You currently have rounding-up donations off.
+        </p>
+      );
+    }
+  };
+
+  render() {
+    console.log('render state', this.state);
+    let mainContent;
+    if (this.state.loaded) {
+      mainContent = (
+        <div class='row'>
+          <div class='col'>
+            <h1 class='landingH1'>Building Karma on autopilot.</h1>
+
+            {this.isActiveText()}
+            <div class='buttons' onClick={this.handleClick}>
+              <button class='play-pause-button'>
+                <i>P</i>
+                <i>l</i>
+                <i>a</i>
+                <i>y</i>
+                <i>use</i>
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      mainContent = (
+        <div class='row'>
+          <div class='col'></div>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <Navbar />
+        <section class='landing'>{mainContent}</section>
+        <section class='info'>
+          <div class='row'>
+            <div class='col bios'>
+              <i class='fas fa-shield-alt'></i>
+              <h1>Secure</h1>
+              <p>
+                Powered by Plaid, you can trust that your banking data is safe.
+                We don't have access to any of your account information.
+              </p>
+            </div>
+            <div class='col'>
+              <div class='bios'>
+                <i class='fas fa-search-dollar'></i>
+                <h1>Transparent</h1>
+                <p>
+                  You have direct control over where your money goes and can
+                  disable donations at any time.
+                </p>
+              </div>
+            </div>
+            <div class='col bios'>
+              <i class='fas fa-hand-holding-usd'></i>
+              <h1>Non-profit</h1>
+              <p>
+                We don't make a cent off of your goodwill. Every penny goes
+                right to the charity of your choice.
+              </p>
+            </div>
+          </div>
+        </section>
+        <div class='wave'>
+          <svg
+            viewBox='0 0 500 150'
+            preserveAspectRatio='none'
+            class='svgStyle'
+          >
+            <path
+              d='M0.00,92.27 C216.83,192.92 304.30,8.39 500.00,109.03 L500.00,0.00 L0.00,0.00 Z'
+              class='pathStyle'
+            ></path>
+          </svg>
+        </div>
+        <section class='moreInfo'>
+          <div class='row'>
+            <div class='col'>
+              <div class='slider'>
+                <h1 class='landingH1'>Most major banks accepted</h1>
+                <div class='slider-row'></div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Home;
